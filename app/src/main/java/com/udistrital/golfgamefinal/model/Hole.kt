8ca2fa1c31b3fa494742e5ba8567ef1349937125
Michael-112823 <maacunag@udistrital.edu.co>
2026-09-11 18:08:54 -0500

    correccion en la clase Hole

--- app/src/main/java/com/udistrital/golfgamefinal/model/Hole.kt
+++ app/src/main/java/com/udistrital/golfgamefinal/model/Hole.kt
@@ -1,7 +1,7 @@
 class Hole (val posicion: Vector2D){
     fun containsBall(ballPos: Vector2D): Boolean{
     val hoyo = 0.5
-        var distancia = posicion.distanceTo(ballPos)
+        val distancia = posicion.distanceTo(ballPos)
         return distancia <= hoyo
     }
 }
