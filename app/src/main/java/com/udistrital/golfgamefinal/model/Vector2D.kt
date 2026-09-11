af7a5157c3f4b5eee4553ac8a53876434a568d4c
Michael-112823 <maacunag@udistrital.edu.co>
2026-09-11 17:20:21 -0500

    Funciones de la clase Vector2D completada

--- app/src/main/java/com/udistrital/golfgamefinal/model/Vector2D.kt
+++ app/src/main/java/com/udistrital/golfgamefinal/model/Vector2D.kt
@@ -1,14 +1,20 @@
 data class Vector2D(val x: Float, val y: Float){
-    fun distanceTo(other: Vector2D){
-
+    fun distanceTo(other: Vector2D): Float{
+        val dx =other.x - x
+        val dy = other.y - y
+        val oper = kotlin.math.sqrt(dx * dx + dy * dy)
+        return oper
     }
 
-    fun plus(deltaX: Float, deltaY: Float){
-
+    fun plus(deltaX: Float, deltaY: Float): Vector2D{
+        val xt = deltaX + x
+        val yt = deltaY + y
+        return Vector2D(xt, yt)
     }
 
-    fun magnitude(){
-
+    fun magnitude(): Float{
+        val oper = kotlin.math.sqrt(x * x + y * y)
+        return oper
     }
 
 }
