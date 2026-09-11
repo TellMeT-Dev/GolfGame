315db0381f7e785b9774f525f28c889f889129c3
Michael-112823 <maacunag@udistrital.edu.co>
2026-09-11 17:51:26 -0500

    Funcion de Hole completada

--- app/src/main/java/com/udistrital/golfgamefinal/model/Hole.kt
+++ app/src/main/java/com/udistrital/golfgamefinal/model/Hole.kt
@@ -1,5 +1,5 @@
-class Hole (){
-    fun containsBall(ballPos: Float){
-
+class Hole (val posicion: Vector2D){
+    fun containsBall(ballPos: Vector2D): Boolean{
+    return ballPos == posicion
     }
 }
