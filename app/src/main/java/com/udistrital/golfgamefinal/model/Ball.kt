f459992767801eb1bf63a022d82d9004271abea2
Michael-112823 <maacunag@udistrital.edu.co>
2026-09-11 17:42:46 -0500

    Funciones Ball completadas

--- app/src/main/java/com/udistrital/golfgamefinal/model/Ball.kt
+++ app/src/main/java/com/udistrital/golfgamefinal/model/Ball.kt
@@ -1,10 +1,12 @@
-class Ball (){
-    fun reset(){
-
+data class Ball (val posicion_i: Vector2D,
+                 var posicion: Vector2D){
+    fun reset(): Vector2D{
+        return posicion_i
     }
 
-    fun moveTo(newPosition: Float){
-
+    fun moveTo(newPosition: Vector2D): Vector2D{
+        posicion = newPosition
+        return posicion
     }
 
 }
